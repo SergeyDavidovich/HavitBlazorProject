d9a66f5603db70912417822158730ff63e468a92
Sergiusz Kriwobokow <writesd@hotmail.com>
2021-11-03 19:44:06 +0100

    Initial commit

---
+++ Contracts/System/IDataSeedFacade.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Havit.ComponentModel;
+
+namespace Havit.NewProjectTemplate.Contracts.System
+{
+	[ApiContract]
+	public interface IDataSeedFacade
+	{
+		Task SeedDataProfileAsync(string profileName, CancellationToken cancellationToken = default);
+
+		Task<Dto<string[]>> GetDataSeedProfilesAsync(CancellationToken cancellationToken = default);
+	}
+}
---
+++ Facades/System/DataSeedFacade.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Havit;
+using Havit.Data.Patterns.DataSeeds;
+using Havit.Data.Patterns.DataSeeds.Profiles;
+using Havit.Extensions.DependencyInjection.Abstractions;
+using Havit.NewProjectTemplate.Contracts;
+using Havit.NewProjectTemplate.Contracts.System;
+using Havit.NewProjectTemplate.DataLayer.Seeds.Core;
+using Havit.NewProjectTemplate.Facades.Infrastructure.Security;
+using Havit.NewProjectTemplate.Facades.Infrastructure.Security.Authorization;
+using Havit.NewProjectTemplate.Model.Security;
+using Havit.NewProjectTemplate.Services.Infrastructure;
+using Havit.Services.Caching;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Havit.NewProjectTemplate.Facades.System
+{
+	[Service]
+	[Authorize(Roles = nameof(Role.Entry.SystemAdministrator))]
+
+	public class DataSeedFacade : IDataSeedFacade
+	{
+		private readonly IDataSeedRunner dataSeedRunner;
+		private readonly ICacheService cacheService;
+
+		public DataSeedFacade(
+			IDataSeedRunner dataSeedRunner,
+			ICacheService cacheService)
+		{
+			this.dataSeedRunner = dataSeedRunner;
+			this.cacheService = cacheService;
+		}
+
+		/// <summary>
+		/// Executes seed for the selected profile.
+		/// </summary>
+		public Task SeedDataProfileAsync(string profileName, CancellationToken cancellationToken = default)
+		{
+			// applicationAuthorizationService.VerifyCurrentUserAuthorization(Operations.SystemAdministration); // TODO alternative authorization approach
+
+			Type type = GetProfileTypes().FirstOrDefault(item => String.Equals(item.Name, profileName, StringComparison.InvariantCultureIgnoreCase));
+
+			if (type == null)
+			{
+				throw new OperationFailedException($"DataSeedProfile {profileName} not found.");
+			}
+
+			// Individual seeds do not invalidate cache. If there are any cached entries (incl. empty-GetAll),
+			// they get seeded and another seed asks for GetAll(), the newly seeded entities are not included.
+			cacheService.Clear();
+
+			dataSeedRunner.SeedData(type, forceRun: true);
+
+			cacheService.Clear();
+
+			return Task.CompletedTask;
+		}
+
+		/// <summary>
+		/// Returns list of available data seed profiles (names are ready for use as parameter to <see cref="SeedDataProfileAsync"/> method).
+		/// </summary>
+		public Task<Dto<string[]>> GetDataSeedProfilesAsync(CancellationToken cancellationToken = default)
+		{
+			return Task.FromResult(Dto.FromValue(GetProfileTypes()
+							.Select(t => t.Name)
+							.ToArray()
+			));
+		}
+
+		private static IEnumerable<Type> GetProfileTypes()
+		{
+			return typeof(CoreProfile).Assembly.GetTypes()
+				.Where(t => t.GetInterfaces().Contains(typeof(IDataSeedProfile)));
+		}
+	}
+}
